75bc20357fc751f29716e11c5dc307badc08fe9b
Stoian Dardzhikov <75749242+StoianDardzhikov@users.noreply.github.com>
2025-12-30 00:30:47 +0200

    Update controls.js

--- frontend/controls-iframe/controls.js
+++ frontend/controls-iframe/controls.js
@@ -71,7 +71,7 @@ class FlipControls {
     }
 
     // Connect to controls namespace
-    this.socket = io('/ws/controls', {
+    this.socket = io('/flip/ws/controls', {
       path: "/flip/socket.io/",    
       query: { sessionId: this.sessionId },
       transports: ['websocket', 'polling']
